e20a73d568a81e61a307f8dda40f9dff95c3189c
deiseda <deiseda@gmail.com>
2019-08-01 15:09:01 -0300

    comitando primeiro post

---
+++ src/test/java/br/com/solutis/gerenciador/GerenciadorFinanceiroApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.solutis.gerenciador;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GerenciadorFinanceiroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
